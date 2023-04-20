0c9a4e33b9bb016fe1c3989e2b6db025e6e8fab8
crisjeanne <crisjeannepador@gmail.com>
2023-04-20 20:33:21 +0800

    Adding Posts CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+(1..10).each do |x|
+    Post.create(title: "Post ##{x}",body: "Body ##{x} 'Body of the post here.'")
+end
